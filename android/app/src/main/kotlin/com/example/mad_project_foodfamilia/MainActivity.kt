db63d17b61edfcd6a19e8d39f2e0773ce2557e54
yuvrajrathod123 <yuvirathod122@gmail.com>
2024-04-02 14:19:17 +0530

    commit
    commite the project

---
+++ android/app/src/main/kotlin/com/example/mad_project_foodfamilia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mad_project_foodfamilia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_selector_macos
+import flutter_timezone
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
+}
